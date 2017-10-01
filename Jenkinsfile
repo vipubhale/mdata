8bd83ef7fa47e61d587e48338cdc501310902708
Vipul Bhale <vipulbhale@gmail.com>
2017-10-01 00:36:32 -0400

    Modified Jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,5 +1,8 @@
 node {
     git([url: 'https://github.com/vipubhale/mdata', branch: 'AddingTesting'])
       def mvnHome = tool 'maven_3.3.9'
-        sh "${mvnHome}/bin/mvn -B verify"
+        sh "${mvnHome}/bin/mvn -Dtest=aug.manas.mtconnect.mdata.unit.UTestMDataService test"
+        sh "${mvnHome}/bin/mvn -Dtest=aug.manas.mtconnect.mdata.itest.ITestMDataController test"
+        sh "${mvnHome}/bin/mvn clean package"
+      
 }
--- src/test/java/aug/manas/mtconnect/mdata/itest/ITestMDataController.java
+++ src/test/java/aug/manas/mtconnect/mdata/itest/ITestMDataController.java
@@ -7,9 +7,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.io.File;
